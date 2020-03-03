e2033fd4670704b7db2b57e36219c8bb5daeb07a
Kirill <lemeshonok12@gmail.com>
2020-03-03 20:08:57 +0700

    lab2: remove useless config build param of spark session

--- lab2/src/data_preparer.py
+++ lab2/src/data_preparer.py
@@ -5,7 +5,6 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder \
     .master("local") \
     .appName("Word Count") \
-    .config("spark.some.config.option", "some-value") \
     .getOrCreate()
 
 df = spark.read.csv(
